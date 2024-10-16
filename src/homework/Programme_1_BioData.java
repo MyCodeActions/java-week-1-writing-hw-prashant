9b52cad82b4be49bc92a60efe0d210b58a0cade1
MyCodeActions <just4you2614@gmail.com>
2024-10-16 16:24:08 -0500

    Writing HomeWork

---
+++ src/homework/Programme_1_BioData.java
@@ -0,0 +1,24 @@
+package homework;
+/**
+ * 1. Write a program that prints your bio data
+ */
+public class Programme_1_BioData {
+
+    public static void main(String[] args) {
+
+        System.out.println("              BIO DATA              ");
+        System.out.println("Name              : Jane Smith");
+        System.out.println("Mobile            : 07123456789");
+        System.out.println("Email             : jamesmith@gmail.com");
+        System.out.println("Gender            : Female");
+        System.out.println("Date of Birth     : 01.05.2000");
+        System.out.println("Marital Status    : Single");
+        System.out.println("Languages Known   : English, Spanish and German");
+        System.out.println("Qualification     : Software Tester");
+        System.out.println("Experience        : Agile Tester with 5 years of experience");
+        System.out.println("Address           : 123 Abc Street, London AB1 2CD");
+        System.out.println("Place             : London");
+        System.out.println("Date              : 02.10.2021");
+        System.out.println("Signature         : Jsmith");
+    }
+}
